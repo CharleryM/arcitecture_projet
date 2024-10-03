4c1137cb4cb7116c01ce8e395a2e5662e381050b
Léo Bacquart <leobacquart@MacBook-Air-de-Leo.local>
2024-10-03 13:35:17 +0200

    Components

---
+++ src/components/AuthCallback.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Login successful</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // Get the access token from the URL hash
+    const hash = window.location.hash
+    const token = hash
+      .split('&')
+      .find((elem) => elem.startsWith('access_token'))
+      .split('=')[1]
+
+    if (token) {
+      // Store the token (e.g., in localStorage or Vuex)
+      localStorage.setItem('accessToken', token)
+
+      // Optionally redirect to another page after login
+      this.$router.push('/home') // or any other route
+    }
+  }
+}
+</script>
---
+++ src/components/AuthComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      const loginUrl =
+        'https://spendwise.auth.eu-west-3.amazoncognito.com/oauth2/authorize?response_type=code&client_id=7ds8aruedeqf5jotdl6ung76n5&redirect_uri=https://dgklnacx3mgmx.cloudfront.net'
+      window.location.href = loginUrl
+    }
+  }
+}
+</script>
